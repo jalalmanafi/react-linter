c8fd403d6576b6144f10ecd6390d9e0bf4fc7d28
Calal B. Manafi <Calal.Manafi@kapitalbank.az>
2022-06-02 12:28:22 +0400

    Added stylelint rules

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,69 @@
 import React from "react";
-import logo from "./logo.svg";
+
 import "./App.scss";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edi
-          <code>src/App.tsx</code>
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <header>Header</header>
+      <nav className="nav">
+        Navbar
+        <input className="nav-search__input" type="text" placeholder="Search" />
+      </nav>
+      <main>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat,
+        dicta ratione, quam dolores animi mollitia iste enim laborum veniam fugit rerum?
+        Excepturi animi nobis, ipsum inventore facere aperiam et ratione eos quae aliquid nisi
+        necessitatibus, sunt
+        accusamus nihil rem. Nisi eius distinctio maxime illum repudiandae debitis quaerat maiores
+        voluptatum similique quo rem tempora amet, v
+        el pariatur delectus nostrum repellat, doloribus facilis enim corporis! Hic recusandae ut
+        quasi deleniti praesentium nobis dolore aspernatur,
+        voluptate id rem quod laborum magni cum consequatur
+        inventore exercitationem illo pariatur nesciunt labore magnam alias enim? Eum, quia unde
+        aliquid dolor id autem accusantium excepturi aperiam possimus?
+        Lorem ipsum dolor sit amet consectetur adipisicing
+        elit. Ad incidunt temporibus ipsa rerum. Voluptate
+        s dolorum tenetur autem unde ipsam, debitis similique hi
+        c dolores maxime sint nesciunt, impedit rerum et natus molestias amet explicab
+        o iure reprehenderit provident harum pariatur culpa ducimus earum! Quam,
+        sit maiores dolor in aspernatur eaque deleniti soluta.
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat,
+        dicta ratione, quam dolores animi mollitia iste enim laborum veniam fugit rerum?
+        Excepturi animi nobis, ipsum inventore facere aperiam et ratione eos quae aliquid nisi
+        necessitatibus, sunt
+        accusamus nihil rem. Nisi eius distinctio maxime illum repudiandae debitis quaerat maiores
+        voluptatum similique quo rem tempora amet, v
+        el pariatur delectus nostrum repellat, doloribus facilis enim corporis! Hic recusandae ut
+        quasi deleniti praesentium nobis dolore aspernatur,
+        voluptate id rem quod laborum magni cum consequatur
+        inventore exercitationem illo pariatur nesciunt labore magnam alias enim? Eum, quia unde
+        aliquid dolor id autem accusantium excepturi aperiam possimus?
+        Lorem ipsum dolor sit amet consectetur adipisicing
+        elit. Ad incidunt temporibus ipsa rerum. Voluptate
+        s dolorum tenetur autem unde ipsam, debitis similique hi
+        c dolores maxime sint nesciunt, impedit rerum et natus molestias amet explicab
+        o iure reprehenderit provident harum pariatur culpa ducimus earum! Quam,
+        sit maiores dolor in aspernatur eaque deleniti soluta.
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat,
+        dicta ratione, quam dolores animi mollitia iste enim laborum veniam fugit rerum?
+        Excepturi animi nobis, ipsum inventore facere aperiam et ratione eos quae aliquid nisi
+        necessitatibus, sunt
+        accusamus nihil rem. Nisi eius distinctio maxime illum repudiandae debitis quaerat maiores
+        voluptatum similique quo rem tempora amet, v
+        el pariatur delectus nostrum repellat, doloribus facilis enim corporis! Hic recusandae ut
+        quasi deleniti praesentium nobis dolore aspernatur,
+        voluptate id rem quod laborum magni cum consequatur
+        inventore exercitationem illo pariatur nesciunt labore magnam alias enim? Eum, quia unde
+        aliquid dolor id autem accusantium excepturi aperiam possimus?
+        Lorem ipsum dolor sit amet consectetur adipisicing
+        elit. Ad incidunt temporibus ipsa rerum. Voluptate
+        s dolorum tenetur autem unde ipsam, debitis similique hi
+        c dolores maxime sint nesciunt, impedit rerum et natus molestias amet explicab
+        o iure reprehenderit provident harum pariatur culpa ducimus earum! Quam,
+        sit maiores dolor in aspernatur eaque deleniti soluta.
+      </main>
+      <footer>Footer</footer>
     </div>
   );
 }
