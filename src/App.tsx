b1ca9110ac9cdc4e25b43ec08e3c1f4ddcbd3e07
jalalbmnf <calalmnf@gmail.com>
2025-04-22 17:33:30 +0400

    🎨 Added TailwindCSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 function App() {
   return (
-    <div className="container">
-      <header>Header</header>
-      <nav className="nav">
-        Navbar
-        <input className="nav-search__input" type="text" placeholder="Search" />
+    <div className="h-screen flex flex-col justify-between bg-slate-50">
+      <header className="h-20 py-5 text-2xl text-center font-bold">
+        Header
+      </header>
+      <nav className="h-16 overflow-x-scroll px-10 py-2 flex justify-start items-center gap-3 border border-l-0 border-r-0 border-slate-200">
+        <span className="text-xl font-bold text-slate-800">Navbar</span>
+        <input
+          className="min-w-3xs px-4 py-2 border rounded-xl border-slate-200 placeholder:text-slate-800 outline-0"
+          type="text"
+          placeholder="Search"
+        />
       </nav>
-      <main>
+      <main className="py-5 px-10">
         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, dicta
         ratione, quam dolores animi mollitia iste enim laborum veniam fugit
         rerum? Excepturi animi nobis, ipsum inventore facere aperiam et ratione
@@ -56,7 +62,9 @@ function App() {
         explicab o iure reprehenderit provident harum pariatur culpa ducimus
         earum! Quam, sit maiores dolor in aspernatur eaque deleniti soluta.
       </main>
-      <footer>Footer</footer>
+      <footer className="h-16 px-10 py-5 text-2xl text-center font-bold flex justify-start items-center gap-3 border-t border-t-slate-200">
+        Footer
+      </footer>
     </div>
   );
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [tailwindcss(), react()],
   resolve: {
     alias: {
       "@/": new URL("./src/", import.meta.url).pathname,
