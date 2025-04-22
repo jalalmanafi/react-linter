4ee07f3dcf2210332fa5246c62185751b60ce027
jalalbmnf <calalmnf@gmail.com>
2025-04-22 11:46:42 +0400

    🔥 Removed sass from project

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,3 @@
-import React from "react";
-
-import "./App.scss";
-
 function App() {
   return (
     <div className="container">
@@ -11,58 +7,54 @@ function App() {
         <input className="nav-search__input" type="text" placeholder="Search" />
       </nav>
       <main>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat,
-        dicta ratione, quam dolores animi mollitia iste enim laborum veniam fugit rerum?
-        Excepturi animi nobis, ipsum inventore facere aperiam et ratione eos quae aliquid nisi
-        necessitatibus, sunt
-        accusamus nihil rem. Nisi eius distinctio maxime illum repudiandae debitis quaerat maiores
-        voluptatum similique quo rem tempora amet, v
-        el pariatur delectus nostrum repellat, doloribus facilis enim corporis! Hic recusandae ut
-        quasi deleniti praesentium nobis dolore aspernatur,
-        voluptate id rem quod laborum magni cum consequatur
-        inventore exercitationem illo pariatur nesciunt labore magnam alias enim? Eum, quia unde
-        aliquid dolor id autem accusantium excepturi aperiam possimus?
-        Lorem ipsum dolor sit amet consectetur adipisicing
-        elit. Ad incidunt temporibus ipsa rerum. Voluptate
-        s dolorum tenetur autem unde ipsam, debitis similique hi
-        c dolores maxime sint nesciunt, impedit rerum et natus molestias amet explicab
-        o iure reprehenderit provident harum pariatur culpa ducimus earum! Quam,
-        sit maiores dolor in aspernatur eaque deleniti soluta.
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat,
-        dicta ratione, quam dolores animi mollitia iste enim laborum veniam fugit rerum?
-        Excepturi animi nobis, ipsum inventore facere aperiam et ratione eos quae aliquid nisi
-        necessitatibus, sunt
-        accusamus nihil rem. Nisi eius distinctio maxime illum repudiandae debitis quaerat maiores
-        voluptatum similique quo rem tempora amet, v
-        el pariatur delectus nostrum repellat, doloribus facilis enim corporis! Hic recusandae ut
-        quasi deleniti praesentium nobis dolore aspernatur,
-        voluptate id rem quod laborum magni cum consequatur
-        inventore exercitationem illo pariatur nesciunt labore magnam alias enim? Eum, quia unde
-        aliquid dolor id autem accusantium excepturi aperiam possimus?
-        Lorem ipsum dolor sit amet consectetur adipisicing
-        elit. Ad incidunt temporibus ipsa rerum. Voluptate
-        s dolorum tenetur autem unde ipsam, debitis similique hi
-        c dolores maxime sint nesciunt, impedit rerum et natus molestias amet explicab
-        o iure reprehenderit provident harum pariatur culpa ducimus earum! Quam,
-        sit maiores dolor in aspernatur eaque deleniti soluta.
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat,
-        dicta ratione, quam dolores animi mollitia iste enim laborum veniam fugit rerum?
-        Excepturi animi nobis, ipsum inventore facere aperiam et ratione eos quae aliquid nisi
-        necessitatibus, sunt
-        accusamus nihil rem. Nisi eius distinctio maxime illum repudiandae debitis quaerat maiores
-        voluptatum similique quo rem tempora amet, v
-        el pariatur delectus nostrum repellat, doloribus facilis enim corporis! Hic recusandae ut
-        quasi deleniti praesentium nobis dolore aspernatur,
-        voluptate id rem quod laborum magni cum consequatur
-        inventore exercitationem illo pariatur nesciunt labore magnam alias enim? Eum, quia unde
-        aliquid dolor id autem accusantium excepturi aperiam possimus?
-        Lorem ipsum dolor sit amet consectetur adipisicing
-        elit. Ad incidunt temporibus ipsa rerum. Voluptate
-        s dolorum tenetur autem unde ipsam, debitis similique hi
-        c dolores maxime sint nesciunt, impedit rerum et natus molestias amet explicab
-        o iure reprehenderit provident harum pariatur culpa ducimus earum! Quam,
-        sit maiores dolor in aspernatur eaque deleniti
-        soluta.
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, dicta
+        ratione, quam dolores animi mollitia iste enim laborum veniam fugit
+        rerum? Excepturi animi nobis, ipsum inventore facere aperiam et ratione
+        eos quae aliquid nisi necessitatibus, sunt accusamus nihil rem. Nisi
+        eius distinctio maxime illum repudiandae debitis quaerat maiores
+        voluptatum similique quo rem tempora amet, v el pariatur delectus
+        nostrum repellat, doloribus facilis enim corporis! Hic recusandae ut
+        quasi deleniti praesentium nobis dolore aspernatur, voluptate id rem
+        quod laborum magni cum consequatur inventore exercitationem illo
+        pariatur nesciunt labore magnam alias enim? Eum, quia unde aliquid dolor
+        id autem accusantium excepturi aperiam possimus? Lorem ipsum dolor sit
+        amet consectetur adipisicing elit. Ad incidunt temporibus ipsa rerum.
+        Voluptate s dolorum tenetur autem unde ipsam, debitis similique hi c
+        dolores maxime sint nesciunt, impedit rerum et natus molestias amet
+        explicab o iure reprehenderit provident harum pariatur culpa ducimus
+        earum! Quam, sit maiores dolor in aspernatur eaque deleniti soluta.
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, dicta
+        ratione, quam dolores animi mollitia iste enim laborum veniam fugit
+        rerum? Excepturi animi nobis, ipsum inventore facere aperiam et ratione
+        eos quae aliquid nisi necessitatibus, sunt accusamus nihil rem. Nisi
+        eius distinctio maxime illum repudiandae debitis quaerat maiores
+        voluptatum similique quo rem tempora amet, v el pariatur delectus
+        nostrum repellat, doloribus facilis enim corporis! Hic recusandae ut
+        quasi deleniti praesentium nobis dolore aspernatur, voluptate id rem
+        quod laborum magni cum consequatur inventore exercitationem illo
+        pariatur nesciunt labore magnam alias enim? Eum, quia unde aliquid dolor
+        id autem accusantium excepturi aperiam possimus? Lorem ipsum dolor sit
+        amet consectetur adipisicing elit. Ad incidunt temporibus ipsa rerum.
+        Voluptate s dolorum tenetur autem unde ipsam, debitis similique hi c
+        dolores maxime sint nesciunt, impedit rerum et natus molestias amet
+        explicab o iure reprehenderit provident harum pariatur culpa ducimus
+        earum! Quam, sit maiores dolor in aspernatur eaque deleniti soluta.
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, dicta
+        ratione, quam dolores animi mollitia iste enim laborum veniam fugit
+        rerum? Excepturi animi nobis, ipsum inventore facere aperiam et ratione
+        eos quae aliquid nisi necessitatibus, sunt accusamus nihil rem. Nisi
+        eius distinctio maxime illum repudiandae debitis quaerat maiores
+        voluptatum similique quo rem tempora amet, v el pariatur delectus
+        nostrum repellat, doloribus facilis enim corporis! Hic recusandae ut
+        quasi deleniti praesentium nobis dolore aspernatur, voluptate id rem
+        quod laborum magni cum consequatur inventore exercitationem illo
+        pariatur nesciunt labore magnam alias enim? Eum, quia unde aliquid dolor
+        id autem accusantium excepturi aperiam possimus? Lorem ipsum dolor sit
+        amet consectetur adipisicing elit. Ad incidunt temporibus ipsa rerum.
+        Voluptate s dolorum tenetur autem unde ipsam, debitis similique hi c
+        dolores maxime sint nesciunt, impedit rerum et natus molestias amet
+        explicab o iure reprehenderit provident harum pariatur culpa ducimus
+        earum! Quam, sit maiores dolor in aspernatur eaque deleniti soluta.
       </main>
       <footer>Footer</footer>
     </div>
